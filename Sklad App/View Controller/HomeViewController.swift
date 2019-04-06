b9338595e068d88199fa95b37fda6bf21f5c98ab
IbRoHiM-IUT <movlonov_95@mail.ru>
2019-04-06 11:35:17 +0500

    Initial Commit

---
+++ Sklad App/View Controller/HomeViewController.swift
@@ -0,0 +1,65 @@
+//
+//  HomeViewController.swift
+//  Sklad App
+//
+//  Created by Ibrokhim Movlonov on 4/2/19.
+//  Copyright © 2019 Ibrokhim Movlonov. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    let transition = SlideInTransition()
+    var topView: UIView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "menu"), style: .plain, target: self, action: #selector(menuButton(_:)))
+        
+
+    }
+    
+    @IBAction func menuButton(_ sender: UIBarButtonItem) {
+        guard let menuViewController = storyboard?.instantiateViewController(withIdentifier: "MenuViewController") as? MenuViewController else { return }
+        menuViewController.didTapMenuType = { menuType in
+            print(menuType)
+            self.transitionToNew(menuType)
+        }
+        menuViewController.modalPresentationStyle = .overCurrentContext
+        menuViewController.transitioningDelegate = self
+        present(menuViewController, animated: true)
+        
+    }
+    
+    func transitionToNew(_ menuType: MenuType) {
+        let title = String(describing: menuType).capitalized
+         self.title = title
+        
+        topView?.removeFromSuperview()
+        switch menuType {
+        case .ostatok:
+            let view = UIView()
+            view.backgroundColor = .yellow
+            view.frame = self.view.bounds
+            self.view.addSubview(view)
+            self.topView = view
+        default:
+            break
+        }
+    }
+    
+}
+
+extension HomeViewController: UIViewControllerTransitioningDelegate {
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transition.isPresenting = true
+        return transition
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transition.isPresenting = false
+        return transition
+    }
+}
---
+++ Sklad App/View Controller/LoginViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LoginViewController.swift
+//  Sklad App
+//
+//  Created by Ibrokhim Movlonov on 4/1/19.
+//  Copyright © 2019 Ibrokhim Movlonov. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet var username: UITextField!
+    @IBOutlet var password: UITextField!
+    var loginButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpProperties()
+    }
+    
+    func setUpProperties() {
+        //Textfields
+        username.layer.cornerRadius = username.layer.frame.height/2
+        password.layer.cornerRadius = password.layer.frame.height/2
+        
+        //Login Button
+       /* loginButton.layer.cornerRadius = loginButton.layer.frame.height/2
+        */
+        
+    }
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        
+    }
+    
+    
+    
+
+}
